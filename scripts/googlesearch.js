9f24ed0e76d9dfcdf1a21a36bacce6a6e584705b
Geoffroy Lesage <gefthefrench@gmail.com>
2016-05-28 16:30:19 -0500

    Comments separating sections in script

--- scripts/googlesearch.js
+++ scripts/googlesearch.js
@@ -1,20 +1,34 @@
+/**
+ * Dependencies
+ */
 var fs = require('fs');
 var _ = require('lodash');
 
-var googleurl = 'https://www.google.com/?hl=en';
-
+/**
+ * Parameters passed as arguments to script
+ */
 var system = require('system');
 var search = system.args[1];
 var debug = system.args[2];
 
-var page = new WebPage(),
-    searchSubmited = null;
-
+/**
+ * Page creation & configuration
+ */
+var page = new WebPage();
 page.customHeaders = {
     "User-Agent": "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
     "Accept-Language": "en"
 };
 
+/**
+ * Global vars
+ */
+var googleurl = 'https://www.google.com/?hl=en';
+var searchSubmited = null;
+
+/**
+ * PhantomJS handlers
+ */
 page.onLoadFinished = function() {
     if (debug) {
         page.render('debug/export.png');
